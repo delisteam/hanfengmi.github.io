9acd1e1a5a1956402d389a81d8e80eb9c2462897
hanzhaoke <838570561@qq.com>
2017-11-01 16:58:50 +0800

    web

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 // require('bootstrap-loader');
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import css from '../css/style.css';
-const greeter = require('./greeter.js');
-document.querySelector('#root').appendChild(greeter())
+import '../scss/style.scss';
+// const greeter = require('./greeter.js');
+// document.querySelector('#root').appendChild(greeter())\
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
   devtool: 'eval-source-map',
   entry: __dirname + "/app/main.js",
@@ -18,7 +18,15 @@ module.exports = {
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
       { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+      { test: /\.(svg|png|jpg)(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
+      {test: /\.scss$/,loader: 'style-loader!css-loader!sass-loader'}
+      // { test:require.resolve('jquery'), loader:'expose-loader?$!expose-loader?jQuery'}
     ]
-  }
+  },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            'jQuery':'jquery'
+        }),
+    ]
 };
